113b76dda7edaa7aa339312acd7960d0c5ac227c
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-08-30 13:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let div = document.createElement('div');
+div.id= 'browser-info';
+body.append(div);
+div.innerHTML = "You Are using "+ navigator.appName + " version " + naviagator.appVersion;
